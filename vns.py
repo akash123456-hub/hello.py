528f498ffa1f6a486b8057d3c07cc0a528d9bb6d
akash0542 <akash.m@ipsator.com>
2021-05-31 09:20:21 +0530

    second commit

---
+++ vns.py
@@ -0,0 +1,9 @@
+#thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+#print(thistuple)
+'''
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+'''
---
+++ vns1.py
@@ -0,0 +1,47 @@
+'''
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+'''
+'''
+mystr = "banana"
+myit = iter(mystr)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+'''
+#thislist = ["apple","banana","cherry","orange"]
+#print(thislist)
+#Allow Duplicates
+'''
+thislist = ["apple","banana","apple","banana","cherry"]
+print(thislist)
+'''
+#Access Items
+'''
+thislist = ["apple","banana","cherry"]
+print(thislist[1])
+'''
+#Negative Indexing
+'''
+thislist = ["apple","banana","cherry"]
+print([thislist[-1]])
+'''
+
+#Range of Indexes
+
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[2:5])
+'''
+'''
+thislist = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(thislist[:4])
+
+
+
