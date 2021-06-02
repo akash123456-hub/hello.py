efe5f1f8842a75818c4e7c3ddd0d07d0c765893a
akash0542 <akash.m@ipsator.com>
2021-06-02 09:50:29 +0530

    fourteen commit

--- inheritance02.py
+++ inheritance02.py
@@ -1,12 +1,14 @@
 class Person:
-    def __init__(self,fname,lname):
+    def __init__(self,fname,lname,add,city):
         self.firstname = fname
         self.lastname = lname
+        self.address = add
+        self.city = city
     def printname(self):
-        print(self.firstname,self.lastname)
+        print(self.firstname,self.lastname,self.address,self.city)
 class Student(Person):
-    def __init__(self,fname,lname):
-      Person. __init__(self,fname,lname) 
-x = Student("Akash","Kumar") 
+    def __init__(self,fname,lname,add,city):
+      Person. __init__(self,fname,lname,add,city) 
+x = Student("Akash","Kumar","C27/96 A Jagatganj","varanasi") 
 x.printname()
 
