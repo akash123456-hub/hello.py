9e27c14403faf4291ae53a2a8f9b02b87bdec999
akash0542 <akash.m@ipsator.com>
2021-06-04 08:58:50 +0530

    fouteetwo commit

---
+++ GetterDecorator.py
@@ -0,0 +1,13 @@
+class Employee:
+ company = "Bharat Gas"
+salary = 5600
+salarybonus = 500
+totalSalary = 6100
+@property
+def totalSalary(self):
+      return self.salary + self.salarybonus
+e = Employee()
+print(e.totalSalary)
+
+
+
