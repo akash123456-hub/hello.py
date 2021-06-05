df3fe584a7713e48a1e4a43401ef398422b66938
akash0542 <akash.m@ipsator.com>
2021-06-05 19:23:58 +0530

    vns06 commit

---
+++ Function05.py
@@ -0,0 +1,12 @@
+def greet(name):
+    print("Good evening,"+name)
+
+def mysum(num1,num2):
+    return num1 + num2
+greet("Akash")
+s = mysum(37,37)
+print(s)
+def mysum1(num3,num4):
+    return num3 +num4
+s1 = mysum1(66,66)
+print(s1)
