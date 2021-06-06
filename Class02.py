ed51ca62a34ffdcc0f9160eda3bad68e3fb2a29e
akash0542 <akash.m@ipsator.com>
2021-06-06 21:25:28 +0530

    vns20 commit

---
+++ Class02.py
@@ -0,0 +1,15 @@
+class Employee:
+    Company = "Google"
+    salary = 3000
+    country = "India"
+harry = Employee()
+rashmi = Employee()
+harry.salary = 4000
+
+print(harry.salary)
+print(rashmi.salary)
+print(harry.country)
+print(rashmi.country)
+
+
+
