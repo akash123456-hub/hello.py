6d2ebabe3c4a5959722bfd0705f078de65bb7d6e
akash0542 <akash.m@ipsator.com>
2021-06-07 22:00:28 +0530

    vns23 commit

---
+++ Inheritance06.py
@@ -0,0 +1,29 @@
+class Person( object ):    
+  
+        # __init__ is known as the constructor         
+        def __init__(self, name, idnumber,salary,post):   
+                self.name = name
+                self.idnumber = idnumber
+                self.salary = salary
+                self.post = post
+        def display(self):
+                print(self.name)
+                print(self.idnumber)
+                print(self.salary)
+                print(self.post)
+  
+# child class
+class Employee( Person ):           
+        def __init__(self, name, idnumber, salary, post):
+                self.salary = salary
+                self.post = post
+  
+                # invoking the __init__ of the parent class 
+                Person.__init__(self, name, idnumber,salary,post) 
+  
+                  
+# creation of an object variable or an instance
+a = Employee('Rahul', 886012, 200000, "Manager")    
+  
+# calling a function of the class Person using its instance
+a.display() 
