dd7e595e75566340877050617bd00b63881e3a0b
akash0542 <akash.m@ipsator.com>
2021-06-07 08:53:14 +0530

    vns20 commit

---
+++ Constructor.py
@@ -0,0 +1,21 @@
+class Employee:
+    company = "Google"
+
+    def __init__(self, name, salary, subunit):
+        self.name = name
+        self.salary = salary
+        self.subunit = subunit
+        print("Employee is created!") 
+
+    def getDetails(self):
+        print(f"The name of the employee is {self.name}")
+        print(f"The salary of the employee is {self.salary}")
+        print(f"The subunit of the employee is {self.subunit}")
+
+    def getSalary(self, signature):
+        print(f"Salary for this employee working in {self.company} is {self.salary}\n{signature}")
+    
+
+harry = Employee("Harry", 100, "YouTube")
+# harry = Employee() --> This throws an error (missing 3 required positional arguments:)
+harry.getDetails()
