e21533747013b37c07111a8d906970e3d736cc6a
akash0542 <akash.m@ipsator.com>
2021-06-13 09:31:39 +0530

    vns40 commit

---
+++ Employee01.py
@@ -0,0 +1,11 @@
+class Employee:
+    company =  "Maruti Suzuki"
+    salary = 20000
+Anjali = Employee()
+Rohini = Employee()
+Anjali.salary = 10000
+Anjali.company = "MRF"
+print(Anjali.company)
+print(Rohini.salary)
+print(Anjali.company)
+print(Rohini.company)
