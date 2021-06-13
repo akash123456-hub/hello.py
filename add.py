b48d6802063c3de19abd39299fcad8023ced5ac1
akash0542 <akash.m@ipsator.com>
2021-06-13 08:57:08 +0530

    vns38 commit

--- add.py
+++ add.py
@@ -1,10 +1,11 @@
 class Number:
     def sum(self):
-        return self.a + self.b
+        return self.a + self.b + self.c
 
 num = Number()
 num.a = 12
 num.b = 34
+num.c = 66
 s = num.sum()
 print(s)
 
