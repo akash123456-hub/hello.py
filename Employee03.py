5d4507f2af384af46233eeeb0e910ba9c2a7c821
akash0542 <akash.m@ipsator.com>
2021-06-14 21:39:21 +0530

    vns46 commit

--- Employee03.py
+++ Employee03.py
@@ -4,8 +4,8 @@ class Employee:
          self.name = name
          self.product = product
     def getInfo(self):
-        print(f"The name of the programmer is {self.name} and the product is {self.product}")
-akash = Employee("Akash","Skype")
+        print(f"The name of the {self.company} programmer is {self.name} and the product is {self.product}")
+akash = Employee("Akash","Gpay")
 ruby = Employee("Ruby","Youtube")
 akash.getInfo()
 ruby.getInfo()
