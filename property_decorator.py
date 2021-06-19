daffdaa87d672c4e13e7d070f22643b84df3f7b6
akash0542 <akash.m@ipsator.com>
2021-06-19 20:33:45 +0530

    vns60 commit

---
+++ property_decorator.py
@@ -0,0 +1,15 @@
+class Employee:
+    company = "Google"
+    salary = 5600
+    salarybonus = 400
+    @property
+    def totalsalary(self):
+        return self.salary + self.salarybonus
+    @totalsalary.setter
+    def totalsalary(self,val):
+        self.salarybonus = val - self.salary
+e = Employee()
+print(e.totalsalary)
+e.totalsalary = 5800
+print(e.salary)
+print(e.salarybonus)
