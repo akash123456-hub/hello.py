c40641383734caf2e1239d53735cc130f0849a36
akash0542 <akash.m@ipsator.com>
2021-06-26 09:03:40 +0530

    vns70 commit

---
+++ salary01.py
@@ -0,0 +1,11 @@
+class Employee:
+    company = "Google"
+    salary = 30000
+akash = Employee()
+alka = Employee()
+akash.salary = 40000
+alka.company = "Youtube"
+print(akash.salary)
+print(alka.salary)
+print(alka.company)
+print(akash.company)
