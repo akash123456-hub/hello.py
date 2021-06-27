0decf10e95873a31792df4dcc043d8cebf57b153
akash0542 <akash.m@ipsator.com>
2021-06-27 17:15:15 +0530

    vns80 commit

--- salary01.py
+++ salary01.py
@@ -1,6 +1,7 @@
 class Employee:
     company = "Google"
     salary = 30000
+    city = "Varanasi"
 akash = Employee()
 alka = Employee()
 akash.salary = 40000
@@ -9,3 +10,5 @@ print(akash.salary)
 print(alka.salary)
 print(alka.company)
 print(akash.company)
+print(akash.city)
+print(alka.city)
